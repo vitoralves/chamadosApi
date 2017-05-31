fd80f58346bba12c962a09bf80dece96420049ac
vitor <vitor.diogo.live@hotmail.com>
2017-05-31 00:12:59 -0300

    alteração do dataTable

--- src/app/paginas/empresas/empresas.component.ts
+++ src/app/paginas/empresas/empresas.component.ts
@@ -15,155 +15,30 @@ declare var swal: any;
 export class EmpresasComponent implements OnInit {
 
   empresas: any;
-
-  public rows:Array<any> = [];
-  public columns:Array<any> = [
-    {title: 'ID', name: 'id', filtering: {filterString: '', placeholder: 'Filtrar por id'}},
-    {title: 'Nome', name: 'nome', filtering: {filterString: '', placeholder: 'Filtrar por Nome'}},
-    {title: 'Telefone', name: 'telefone', filtering: {filterString: '', placeholder: 'Filtrar por Telefone'}},
-    {title: 'Celular', name: 'celular', filtering: {filterString: '', placeholder: 'Filtrar por Celular'}},
-    {title: 'Email', name: 'email', filtering: {filterString: '', placeholder: 'Filtrar por Email'}},
-    {title: 'Endereço', name: 'endereco', filtering: {filterString: '', placeholder: 'Filtrar por Endereço'}},
-  ];
-
-  public page:number = 1;
-  public itemsPerPage:number = 10;
-  public maxSize:number = 5;
-  public numPages:number = 1;
-  public length:number = 0;
-
-  public config:any = {
-    paging: true,
-    sorting: {columns: this.columns},
-    filtering: {filterString: ''},
-    className: ['table-striped', 'table-bordered']
-  };
-
-  private data:Array<any>;
+  pesquisaParam: any;
+  empresasPesquisa: any;
 
   constructor(private rootComp: AppComponent, private service: EmpresasService, private rota: Router) {
     this.rootComp.cssClass = 'hold-transition skin-blue-light sidebar-mini';
   }
 
   ngOnInit() {
-    this.iniciarTabela();
+    this.buscaTodasEmpresas();
   }
 
-  public iniciarTabela(){
+  buscaTodasEmpresas(){
     this.service.retornaTodasEmpresas().then(data => {
-      this.data = data.data;
-      this.length = data.data.length;
-      this.onChangeTable(this.config);
-    });
+      this.empresas = data.data;
+      this.empresasPesquisa = this.empresas;
+    })
   }
 
   novo() {
     this.rota.navigate(['/pages/empresas/novo']);
   }
 
-  public changePage(page:any, data:Array<any> = this.data):Array<any> {
-    let start = (page.page - 1) * page.itemsPerPage;
-    let end = page.itemsPerPage > -1 ? (start + page.itemsPerPage) : data.length;
-    return data.slice(start, end);
-  }
-
-  public changeSort(data:any, config:any):any {
-    if (!config.sorting) {
-      return data;
-    }
-
-    let columns = this.config.sorting.columns || [];
-    let columnName:string = void 0;
-    let sort:string = void 0;
-
-    for (let i = 0; i < columns.length; i++) {
-      if (columns[i].sort !== '' && columns[i].sort !== false) {
-        columnName = columns[i].name;
-        sort = columns[i].sort;
-      }
-    }
-
-    if (!columnName) {
-      return data;
-    }
-
-    // simple sorting
-    return data.sort((previous:any, current:any) => {
-      if (previous[columnName] > current[columnName]) {
-        return sort === 'desc' ? -1 : 1;
-      } else if (previous[columnName] < current[columnName]) {
-        return sort === 'asc' ? -1 : 1;
-      }
-      return 0;
-    });
-  }
-
-  public changeFilter(data:any, config:any):any {
-    let filteredData:Array<any> = data;
-    this.columns.forEach((column:any) => {
-      if (column.filtering) {
-        filteredData = filteredData.filter((item:any) => {
-          if (item[column.name] === null){
-            var array = [
-              {
-                "0": 0,
-                "index": 0,
-                "input": ""
-              }
-            ];
-            return array;
-          }else{
-            return item[column.name].toString().match(column.filtering.filterString);
-          }
-        });
-      }
-    });
-
-    if (!config.filtering) {
-      return filteredData;
-    }
-
-    if (config.filtering.columnName) {
-      return filteredData.filter((item:any) =>
-      item[config.filtering.columnName].match(this.config.filtering.filterString));
-    }
-
-    let tempArray:Array<any> = [];
-    filteredData.forEach((item:any) => {
-      let flag = false;
-      this.columns.forEach((column:any) => {
-        if(item[column.name] === null){
-          flag = false;
-        }else if (item[column.name].toString().match(this.config.filtering.filterString)) {
-          flag = true;
-        }
-      });
-      if (flag) {
-        tempArray.push(item);
-      }
-    });
-    filteredData = tempArray;
-
-    return filteredData;
-  }
-
-  public onChangeTable(config:any, page:any = {page: this.page, itemsPerPage: this.itemsPerPage}):any {
-    if (config.filtering) {
-      Object.assign(this.config.filtering, config.filtering);
-    }
-
-    if (config.sorting) {
-      Object.assign(this.config.sorting, config.sorting);
-    }
-
-    let filteredData = this.changeFilter(this.data, this.config);
-    let sortedData = this.changeSort(filteredData, this.config);
-    this.rows = page && config.paging ? this.changePage(page, sortedData) : sortedData;
-    this.length = sortedData.length;
-  }
-
-  public onCellClick(data: any) {
-    this.rota.navigate(['pages/empresas/', data.row.id]);
+  editar(id: number){
+    this.rota.navigate(['pages/empresas/', id]);
   }
 
   public mostraAlert(): Promise<any>{
@@ -183,10 +58,10 @@ export class EmpresasComponent implements OnInit {
     })
   }
 
-  public deletar(data: any){
+  public deletar(id: number){
     this.mostraAlert().then(retorno => {
       if (retorno) {
-        this.apagarItem(data.row.id);
+        this.apagarItem(id);
       }
     })
   }
@@ -199,7 +74,8 @@ export class EmpresasComponent implements OnInit {
           'O item de código '+id+' foi apagado!',
           'success'
         )
-        this.iniciarTabela();
+        //se necessitar de melhor processamento remover somente do array ao invéz de solicitar na API
+        this.buscaTodasEmpresas();
       }else{
         swal(
           'Erro!',
@@ -208,6 +84,18 @@ export class EmpresasComponent implements OnInit {
         )
       }
     })
+  }
+
+  pesquisar(){
+    if (!this.pesquisaParam){
+      this.empresas = this.empresasPesquisa;
+    }else if (!isNaN(this.pesquisaParam)){
+      this.empresas = this.empresasPesquisa;
+      this.empresas = this.empresas.filter(item => item.id === +this.pesquisaParam);
+    }else{
+      this.empresas = this.empresasPesquisa;
+      this.empresas = this.empresas.filter(item => item.nome.toUpperCase().indexOf(this.pesquisaParam.toUpperCase()) !== -1);
+    }
   }
 
 }
