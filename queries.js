f50547b584e136e87341c37cdbe9959bc2153cb1
vitor <vitor.diogo.live@hotmail.com>
2017-06-25 19:49:04 -0300

    usuarios

--- queries.js
+++ queries.js
@@ -5,372 +5,453 @@ var options = {
 };
 
 var pgp = require('pg-promise')(options);
-var connectionString = 'postgres://postgres:a@localhost:5432/chamados';
+var connectionString = 'postgres://postgres:info2013@localhost:5432/chamados';
 var db = pgp(connectionString);
 
-function tokenValido(t){
-    var data = new Date();
-    var token = ((data.getMonth() + 1) * 100) * data.getDate();
-    if (t == token){
-      return true;
-    }
+function tokenValido(t) {
+  var data = new Date();
+  var token = ((data.getMonth() + 1) * 100) * data.getDate();
+  if (t == token) {
+    return true;
+  }
 
-    return false;
+  return false;
 }
 
 // ***************************
 // USUARIO
 // ****************************
-function registrar(req, res, next){
+function registrar(req, res, next) {
   var obj = req.params.obj;
-  console.log("objto"+obj);
+  console.log("objto" + obj);
   var reg = JSON.parse(obj);
-  console.log("json"+reg);
+  console.log("json" + reg);
 
   db.query('insert into usuario(nome,sobrenome,cpf,email,senha) values($1,$2,$3,$4,md5($5)) returning id',
-  [reg.usuNome,reg.usuSobrenome,reg.usuCpf,reg.usuEmail,reg.usuSenha])
-  .then(function (data){
-    let id = data[0].id;
-    console.log('id '+id);
-    db.query('insert into fornecedor(nome,cnpj,endereco,telefone,celular,email,cidade,estado,usuario,cep,bairro,complemento) values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)',
-    [reg.fornNome, reg.fornCnpj, reg.fornEndereco, reg.fornTelefone, reg.fornCelular, reg.fornEmail,reg.fornCidade,reg.fornEstado,id,reg.fornCep,reg.fornBairro,reg.fornComplemento])
-    .then(function (){
-      res.status(200)
-      .json({
-        status: 'succes'
-      });
+    [reg.usuNome, reg.usuSobrenome, reg.usuCpf, reg.usuEmail, reg.usuSenha])
+    .then(function (data) {
+      let id = data[0].id;
+      console.log('id ' + id);
+      db.query('insert into fornecedor(nome,cnpj,endereco,telefone,celular,email,cidade,estado,usuario,cep,bairro,complemento) values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)',
+        [reg.fornNome, reg.fornCnpj, reg.fornEndereco, reg.fornTelefone, reg.fornCelular, reg.fornEmail, reg.fornCidade, reg.fornEstado, id, reg.fornCep, reg.fornBairro, reg.fornComplemento])
+        .then(function () {
+          res.status(200)
+            .json({
+              status: 'succes'
+            });
+        })
     })
-  })
-  .catch(function (e){
-    return next(e);
-  })
+    .catch(function (e) {
+      return next(e);
+    })
 }
 
-function updatePerfil(req, res, next){
+function updatePerfil(req, res, next) {
   var usuario = req.params.usu;
   var token = req.params.token;
   var u = JSON.parse(usuario);
 
-  if(tokenValido(token)){
-    db.query('update usuarios set nome = $1, email = $2, imagem = $4 where id = $3', [u.nome, u.email, u.id, u.imagem])
-    .then(function (){
-      res.status(200)
-      .json({
-        message: 'sucesso'
+  if (tokenValido(token)) {
+    db.query('update usuarios set nome = $1, email = $2 where id = $3', [u.nome, u.email, u.id])
+      .then(function () {
+        res.status(200)
+          .json({
+            message: 'sucesso'
+          });
+      }).catch(function (e) {
+        res.status(500)
+          .json({
+            status: 'error',
+            message: '' + e
+          })
       });
-    }). catch(function (e){
-      res.status(500)
-      .json({
-        status: 'error',
-        message: ''+e
-      })
-    });
-  }else{
+  } else {
     res.status(406)
-    .json({
-      status: 'Not Acceptable',
-      message: 'token inválido!'
-    });
+      .json({
+        status: 'Not Acceptable',
+        message: 'token inválido!'
+      });
   }
 }
 
-function updatePerfilFoto(req, res, next){
-  var img = JSON.parse(req.params.img);
-
-  
-  db.query('update usuarios set imagem = $1', img)
-  .then(function (){
-    res.status(200)
-    .json({
-      message: 'sucesso'
-    });
-  }). catch(function (e){
-    res.status(500)
-    .json({
-      status: 'error',
-      message: ''+e
-    })
-  });
-
+function updatePerfilFoto(req, res, next) {
+  var img = req.params.img;
+  console.log(img);
+  /*db.query('update usuarios set imagem = $1', r)
+    .then(function () {
+      res.status(200)
+        .json({
+          message: 'sucesso'
+        });
+    }).catch(function (e) {
+      res.status(500)
+        .json({
+          status: 'error',
+          message: '' + e
+        })
+    });*/
 }
 
-function getUsuario(req, res, next){
+function getUsuario(req, res, next) {
   var e = req.params.email;
   var s = req.params.senha;
   var token = req.params.token;
 
-  if (tokenValido(token)){
+  if (tokenValido(token)) {
     console.log('valido');
     db.any('select * from usuarios where email = $1 and senha = $2', [e, s])
-    .then(function (data){
-      res.status(200)
-      .json({
-        status: 'success',
-        data: data
+      .then(function (data) {
+        res.status(200)
+          .json({
+            status: 'success',
+            data: data
+          });
+      })
+      .catch(function (err) {
+        res.status(500)
+          .json({
+            status: 'error',
+            message: '' + err
+          });
       });
-    })
-    .catch(function (err){
-      res.status(500)
-      .json({
-        status: 'error',
-        message: ''+err
-      });
-    });
-  }else{
+  } else {
     res.status(406)
-    .json({
-      status: 'Not Acceptable',
-      message: 'token inválido!'
-    });
+      .json({
+        status: 'Not Acceptable',
+        message: 'token inválido!'
+      });
   }
 }
 
-function getUsuarioPorId(req, res, next){
-  var token = req.params.token;
-  var id = req.params.id;
-
-  if(tokenValido(token)){
-    db.one('select * from usuarios where id = $1', id)
-    .then(function (data){
-      res.status(200)
-      .json({
-        data:data
-      });
-    }).catch(function (e){
-      return next(e);
-    });
-  }else{
-    res.status(406)
-    .json({
-      status:'Not acceptable',
-      message: 'token inválido!'
-    });
-  }
-}
 
 // **************
 // EMPRESA
 // **************
-function updateEmpresa(req, res, next){
+function updateEmpresa(req, res, next) {
   var objeto = JSON.parse(req.params.empresa);
-  db.query('update empresas set nome = $1, telefone = $2, email = $3, endereco = $4, celular = $5 where id = $6', [objeto.nome, objeto.telefone, objeto.email, objeto. endereco, objeto.celular, objeto.id])
-  .then(function (f){
-    res.status(200)
-    .json({
-      status: 'success'
+  db.query('update empresas set nome = $1, telefone = $2, email = $3, endereco = $4, celular = $5 where id = $6', [objeto.nome, objeto.telefone, objeto.email, objeto.endereco, objeto.celular, objeto.id])
+    .then(function (f) {
+      res.status(200)
+        .json({
+          status: 'success'
+        })
+    }).catch(function (e) {
+      return next(e);
     })
-  }).catch(function (e){
-    return next(e);
-  })
 }
 
-function getEmpresa(req, res, next){
+function getEmpresa(req, res, next) {
   var empresa = req.params.empresa;
   db.one('select * from empresas where id = $1', empresa)
-  .then(function (data){
-    res.status(200)
-    .json({
-      data: data
-    });
-  })
-  .catch(function (err){
-    return next(err);
-  })
-}
-
-function adicionarEmpresa(req, res, next){
-  var objeto = JSON.parse(req.params.empresa);
-  db.query('insert into empresas(nome,telefone,email,endereco) values($1,$2,$3,$4)', [objeto.nome, objeto.telefone, objeto.email, objeto. endereco])
-  .then(function (f){
-    res.status(200)
-    .json({
-      status: 'success'
-    })
-  }).catch(function (e){
-    return next(e);
-  })
-}
-
-function getTodasEmpresas(req, res, next){
-  var token = req.params.token;
-  if(tokenValido(token)){
-    db.any('select * from empresas')
-    .then(function (data){
+    .then(function (data) {
       res.status(200)
-      .json({
-        data: data
-      });
+        .json({
+          data: data
+        });
     })
-    .catch(function (err){
+    .catch(function (err) {
       return next(err);
     })
-  }else{
+}
+
+function adicionarEmpresa(req, res, next) {
+  var objeto = JSON.parse(req.params.empresa);
+  db.query('insert into empresas(nome,telefone,email,endereco) values($1,$2,$3,$4)', [objeto.nome, objeto.telefone, objeto.email, objeto.endereco])
+    .then(function (f) {
+      res.status(200)
+        .json({
+          status: 'success'
+        })
+    }).catch(function (e) {
+      return next(e);
+    })
+}
+
+function getTodasEmpresas(req, res, next) {
+  var token = req.params.token;
+  if (tokenValido(token)) {
+    db.any('select * from empresas')
+      .then(function (data) {
+        res.status(200)
+          .json({
+            data: data
+          });
+      })
+      .catch(function (err) {
+        return next(err);
+      })
+  } else {
     res.status(406)
-    .json({
-      status:'Not acceptable',
-      message: 'token inválido!'
-    });
+      .json({
+        status: 'Not acceptable',
+        message: 'token inválido!'
+      });
   }
 }
 
-function deleteEmpresa(req, res, next){
+function deleteEmpresa(req, res, next) {
   var id = req.params.id;
   db.query('delete from empresas where id = $1', id)
-  .then(function (f){
-    res.status(200)
-    .json({
-      status: 'success'
-    })
-  }).catch(function (e){
+    .then(function (f) {
+      res.status(200)
+        .json({
+          status: 'success'
+        })
+    }).catch(function (e) {
       res.status(500)
-      .json({
-        status: 'error',
-        message: ''+e
-      });
-  })
+        .json({
+          status: 'error',
+          message: '' + e
+        });
+    })
 }
 
-function getEmpresasProduto(req, res, next){
+function getEmpresasProduto(req, res, next) {
   var id = req.params.id;
   db.query('select ep.id, p.nome, ep.ativo from empresas_produtos ep join produtos p on p.id = ep.produto where empresa = $1', id)
-  .then(function (data){
-    res.status(200)
-    .json({
-      data: data
+    .then(function (data) {
+      res.status(200)
+        .json({
+          data: data
+        })
+    }).catch(function (e) {
+      res.status(500)
+        .json({
+          status: 'error',
+          message: '' + e
+        })
     })
-  }).catch(function (e){
-    res.status(500)
-    .json({
-      status: 'error',
-      message: ''+e
-    })
-  })
 }
 
-function addEmpresasProdutos(req, res, next){
+function addEmpresasProdutos(req, res, next) {
   var objeto = JSON.parse(req.params.empresaProduto);
   db.query('insert into empresas_produtos(produto, empresa, ativo) values($1,$2,$3)', [objeto.produto, objeto.empresa, objeto.ativo])
-  .then(function (f){
-    res.status(200)
-    .json({
-      status: 'success'
+    .then(function (f) {
+      res.status(200)
+        .json({
+          status: 'success'
+        })
+    }).catch(function (e) {
+      res.status(500)
+        .json({
+          status: 'error',
+          message: '' + e
+        })
     })
-  }).catch(function (e){
-    res.status(500)
-    .json({
-      status: 'error',
-      message: ''+e
-    })
-  })
 }
 
-function updateEmpresasProdutos(req, res, next){
+function updateEmpresasProdutos(req, res, next) {
   var objeto = JSON.parse(req.params.empresaProduto);
   db.query('update empresas_produtos set produto = $1, set empresa = $2, set ativo = $3 where id = $4) values($1,$2,$3)', [objeto.produto, objeto.empresa, objeto.ativo, objeto.id])
-  .then(function (f){
-    res.status(200)
-    .json({
-      status: 'success'
+    .then(function (f) {
+      res.status(200)
+        .json({
+          status: 'success'
+        })
+    }).catch(function (e) {
+      res.status(500)
+        .json({
+          status: 'error',
+          message: '' + e
+        })
     })
-  }).catch(function (e){
-    res.status(500)
-    .json({
-      status: 'error',
-      message: ''+e
-    })
-  })
 }
 
-function deleteEmpresasProdutos(req, res, next){
+function deleteEmpresasProdutos(req, res, next) {
   var id = req.params.id;
   db.query('delete from empresas_produtos where id = $1', id)
-  .then(function (f){
-    res.status(200)
-    .json({
-      status: 'success'
-    })
-  }).catch(function (e){
+    .then(function (f) {
+      res.status(200)
+        .json({
+          status: 'success'
+        })
+    }).catch(function (e) {
       res.status(500)
-      .json({
-        status: 'error',
-        message: ''+e
-      });
-  })
+        .json({
+          status: 'error',
+          message: '' + e
+        });
+    })
 }
 
 // ***********************
 // PRODUTOS
 // **********************
-function updateProduto(req, res, next){
+function updateProduto(req, res, next) {
   var objeto = JSON.parse(req.params.produto);
   db.query('update produtos set nome = $1, ativo = $2 where id = $3', [objeto.nome, objeto.ativo, objeto.id])
-  .then(function (f){
-    res.status(200)
-    .json({
-      status: 'success'
+    .then(function (f) {
+      res.status(200)
+        .json({
+          status: 'success'
+        })
+    }).catch(function (e) {
+      return next(e);
     })
-  }).catch(function (e){
-    return next(e);
-  })
 }
 
-function getProduto(req, res, next){
+function getProduto(req, res, next) {
   var produto = req.params.produto;
   db.one('select * from produtos where id = $1', produto)
-  .then(function (data){
-    res.status(200)
-    .json({
-      data: data
-    });
-  })
-  .catch(function (err){
-    return next(err);
-  })
-}
-
-function adicionarProduto(req, res, next){
-  var objeto = JSON.parse(req.params.produto);
-  db.query('insert into produtos(nome,ativo) values($1,$2)', [objeto.nome, objeto.ativo])
-  .then(function (f){
-    res.status(200)
-    .json({
-      status: 'success'
-    })
-  }).catch(function (e){
-    return next(e);
-  })
-}
-
-function getTodosProdutos(req, res, next){
-    db.any('select * from produtos')
-    .then(function (data){
+    .then(function (data) {
       res.status(200)
-      .json({
-        data: data
-      });
+        .json({
+          data: data
+        });
     })
-    .catch(function (err){
+    .catch(function (err) {
       return next(err);
     })
 }
 
-function deleteProduto(req, res, next){
-  var id = req.params.id;
-  db.query('delete from produtos where id = $1', id)
-  .then(function (f){
-    res.status(200)
-    .json({
-      status: 'success'
-    })
-  }).catch(function (e){
+function adicionarProduto(req, res, next) {
+  var objeto = JSON.parse(req.params.produto);
+  db.query('insert into produtos(nome,ativo) values($1,$2)', [objeto.nome, objeto.ativo])
+    .then(function (f) {
       res.status(200)
-      .json({
-        status: 'error',
-        message: ''+e
-      });
-  })
+        .json({
+          status: 'success'
+        })
+    }).catch(function (e) {
+      return next(e);
+    })
 }
 
+function getTodosProdutos(req, res, next) {
+  db.any('select * from produtos')
+    .then(function (data) {
+      res.status(200)
+        .json({
+          data: data
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    })
+}
+
+function deleteProduto(req, res, next) {
+  var id = req.params.id;
+  db.query('delete from produtos where id = $1', id)
+    .then(function (f) {
+      res.status(200)
+        .json({
+          status: 'success'
+        })
+    }).catch(function (e) {
+      res.status(200)
+        .json({
+          status: 'error',
+          message: '' + e
+        });
+    })
+}
+
+
+// **************
+// USUARIO
+// **************
+function updateUsuario(req, res, next) {
+  var objeto = JSON.parse(req.params.usuario);
+  db.query('update usuarios set nome = $1, email = $2, adm = $3, empresa = $4 where id = $5', [objeto.nome, objeto.email, objeto.adm, objeto.empresa, objeto.id])
+    .then(function (f) {
+      res.status(200)
+        .json({
+          status: 'success'
+        })
+    }).catch(function (e) {
+      return next(e);
+    })
+}
+
+function getUsuarioPorId(req, res, next) {
+  var usuario = req.params.usuario;
+  db.one('select * from usuarios where id = $1', usuario)
+    .then(function (data) {
+      res.status(200)
+        .json({
+          data: data
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    })
+}
+
+function adicionarUsuario(req, res, next) {
+  var objeto = JSON.parse(req.params.usuario);
+  db.query('insert into usuarios(nome,email,adm,empresa,senha) values($1,$2,$3,$4,$5)', [objeto.nome, objeto.email, objeto.adm, objeto.empresa, objeto.senha])
+    .then(function (f) {
+      res.status(200)
+        .json({
+          status: 'success'
+        })
+    }).catch(function (e) {
+      res.status(500)
+        .json({
+          status: 'error',
+          message: '' + e
+        });
+    })
+}
+
+function getTodosUsuarios(req, res, next) {
+  var token = req.params.token;
+  if (tokenValido(token)) {
+    db.any('select u.id,u.nome,u.email,u.adm,u.imagem,e.nome as empresa from usuarios u join empresas e on e.id = u.empresa')
+      .then(function (data) {
+        res.status(200)
+          .json({
+            data: data
+          });
+      })
+      .catch(function (err) {
+        return next(err);
+      })
+  } else {
+    res.status(406)
+      .json({
+        status: 'Not acceptable',
+        message: 'token inválido!'
+      });
+  }
+}
+
+function deleteUsuario(req, res, next) {
+  var id = req.params.id;
+  db.query('delete from usuarios where id = $1', id)
+    .then(function (f) {
+      res.status(200)
+        .json({
+          status: 'success'
+        })
+    }).catch(function (e) {
+      res.status(500)
+        .json({
+          status: 'error',
+          message: '' + e
+        });
+    })
+}
+
+function updateSenha(req, res, next) {
+  var usuario = req.params.id;
+  var senha = req.params.senha;
+
+  db.query('update usuarios set senha = $2 where id = $1', [usuario, senha])
+    .then(function (f) {
+      res.status(200)
+        .json({
+          status: 'success'
+        })
+    }).catch(function (e) {
+      res.status(500)
+        .json({
+          status: 'error',
+          message: '' + e
+        });
+    })
+}
 
 module.exports = {
   // usuario
@@ -394,5 +475,11 @@ module.exports = {
   getTodosProdutos: getTodosProdutos,
   adicionarProduto: adicionarProduto,
   deleteProduto: deleteProduto,
-  updateProduto: updateProduto
+  updateProduto: updateProduto,
+  // usuarios
+  getTodosUsuarios: getTodosUsuarios,
+  adicionarUsuario: adicionarUsuario,
+  deleteUsuario: deleteUsuario,
+  updateUsuario: updateUsuario,
+  updateSenha: updateSenha
 };
--- routes/index.js
+++ routes/index.js
@@ -10,10 +10,9 @@ var db = require('../queries.js');
 
 // usuario
 router.get('/api/logar/:email/:senha/:token', db.getUsuario);
-router.get('/api/usuario/getUsuarioPorId/:id/:token', db.getUsuarioPorId);
-router.get('/api/usuario/getUsuarioPorId/:id/:token', db.getUsuarioPorId);
+router.get('/api/usuario/getUsuarioPorId/:usuario', db.getUsuarioPorId);
 router.put('/api/perfil/salvar/:usu/:token', db.updatePerfil);
-router.put('/api/perfil/salvar/imagem/:img', db.updatePerfilFoto);
+router.put('/api/perfil/imagem/:img', db.updatePerfilFoto);
 // empresa
 router.get('/api/empresa/:empresa', db.getEmpresa);
 router.get('/api/empresas/all/:token', db.getTodasEmpresas);
@@ -24,12 +23,17 @@ router.get('/api/empresas/produtos/:id', db.getEmpresasProduto);
 router.post('/api/empresas/produtos/novo/:empresaProduto', db.addEmpresasProdutos);
 router.put('/api/empresas/produtos/update/:empresaProduto', db.updateEmpresasProdutos);
 router.delete('/api/empresas/produtos/delete/:id', db.deleteEmpresasProdutos);
-
 // produtos
 router.get('/api/produto/:produto', db.getProduto);
 router.get('/api/produtos/all', db.getTodosProdutos);
 router.post('/api/produtos/novo/:produto', db.adicionarProduto);
 router.put('/api/produto/update/:produto', db.updateProduto);
 router.delete('/api/produto/delete/:id', db.deleteProduto);
+//usuarios
+router.get('/api/usuarios/all/:token', db.getTodosUsuarios);
+router.post('/api/usuarios/novo/:usuario', db.adicionarUsuario);
+router.put('/api/usuario/update/:usuario', db.updateUsuario);
+router.delete('/api/usuario/delete/:id', db.deleteUsuario);
+router.put('/api/usuario/senha/:id/:senha', db.updateSenha);
 
 module.exports = router;
